6874d9c98db91c28eae02ac752c3fb760ec2e10d
Guang Shi <guang.a.shi@gmail.com>
2022-12-02 09:43:39 +0800

    pep8 format

--- examples.py
+++ examples.py
@@ -1,13 +1,16 @@
 from horizonserver import HorizonServer
 
+
 def test_enable_login_as_current_user(enabled=True):
     username = r'administrator'
     password = r'ca$hc0w'
     domain = r'wbd'
     address = '10.117.30.135'
-    hs = HorizonServer(username=username, password=password, domain=domain, address=address)
+    hs = HorizonServer(username=username, password=password,
+                       domain=domain, address=address)
     if not hs.gssapi_authenticators:
-        hs.create_gssapi_authenticator(False, False, [cs.id for cs in hs.connection_servers], enabled, False, 'DISABLED')
+        hs.create_gssapi_authenticator(
+            False, False, [cs.id for cs in hs.connection_servers], enabled, False, 'DISABLED')
     else:
         for gssapi_auth in hs.gssapi_authenticators:
             gssapi_auth.enable_login_as_current_user = enabled
--- horizonserver.py
+++ horizonserver.py
@@ -5,13 +5,15 @@ import logging
 import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-logging.basicConfig(level=logging.INFO, format="%(asctime)-15s| %(threadName)-18s| %(message)s", stream=sys.stdout)
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)-15s| %(threadName)-18s| %(message)s", stream=sys.stdout)
 
 
 class HorizonServer:
     """
-    REST API Client for Horizon Connection Server, this is the only public class, create an instance first
+    REST API Client for Horizon Connection Server
     """
+
     def __init__(self, username, password, domain, address):
         self._username = username
         self._password = password
@@ -49,7 +51,8 @@ class HorizonServer:
     def http_get(self, url, params=None, headers=None, verify=False, timeout=None, **kwargs) -> requests.Response:
         if headers is None:
             headers = self._default_headers
-        response = requests.get(url=url, params=params, headers=headers, verify=verify, timeout=timeout, **kwargs)
+        response = requests.get(
+            url=url, params=params, headers=headers, verify=verify, timeout=timeout, **kwargs)
         if response.status_code != 200:
             logging.info('Get[{}]: {}'.format(response.status_code, url))
             logging.info(response.json())
@@ -59,7 +62,8 @@ class HorizonServer:
     def http_delete(self, url, params=None, headers=None, verify=False, timeout=None, **kwargs) -> requests.Response:
         if headers is None:
             headers = self._default_headers
-        response = requests.delete(url=url, params=params, headers=headers, verify=verify, timeout=timeout, **kwargs)
+        response = requests.delete(
+            url=url, params=params, headers=headers, verify=verify, timeout=timeout, **kwargs)
         if response.status_code != 204:
             logging.info('Delete[{}]: {}'.format(response.status_code, url))
             logging.info(response.json())
@@ -68,7 +72,8 @@ class HorizonServer:
 
     def _login(self):
         login_url = f'https://{self.address}/rest/login'
-        data = {"username": self._username, "password": self._password, "domain": self._domain}
+        data = {"username": self._username,
+                "password": self._password, "domain": self._domain}
         response = requests.post(url=login_url, json=data, verify=False)
         data = response.json()
         refresh_token = data['refresh_token']
@@ -117,16 +122,19 @@ class HorizonServer:
         data = dict()
         data['allow_legacy_clients'] = allow_legacy_clients
         data['allow_ntlm_fallback'] = allow_ntlm_fallback
-        data['connection_server_ids'] = [] if not connection_server_ids else connection_server_ids
+        data['connection_server_ids'] = [
+        ] if not connection_server_ids else connection_server_ids
         data['enable_login_as_current_user'] = enable_login_as_current_user
         data['enforce_channel_bindings'] = enforce_channel_bindings
         data['trigger_mode'] = trigger_mode
         response = self.http_post(url=url, json=data)
-        new_gssapi_authenticator_id = response.headers['Location'].split('/')[-1]
+        new_gssapi_authenticator_id = response.headers['Location'].split(
+            '/')[-1]
         return GSSAPIAuthenticator(new_gssapi_authenticator_id, self)
 
     def delete_gssapi_authenticator(self, gssapi_authenticator_id):
-        url = 'https://{}/rest/config/v1/gssapi-authenticators/{}'.format(self.address, gssapi_authenticator_id)
+        url = 'https://{}/rest/config/v1/gssapi-authenticators/{}'.format(
+            self.address, gssapi_authenticator_id)
         self.http_delete(url=url, params={'forced': 'true'})
 
     @property
@@ -146,7 +154,8 @@ class GSSAPIAuthenticator:
     def __init__(self, gssapi_authenticator_id, horizon_server: HorizonServer):
         self.id = gssapi_authenticator_id
         self.horizon_server = horizon_server
-        self.url = 'https://{}/rest/config/v1/gssapi-authenticators/{}'.format(horizon_server.address, self.id)
+        self.url = 'https://{}/rest/config/v1/gssapi-authenticators/{}'.format(
+            horizon_server.address, self.id)
         self._generate_get_response_data()
         self._generate_put_request_data()
 
@@ -173,7 +182,8 @@ class GSSAPIAuthenticator:
     @enable_login_as_current_user.setter
     def enable_login_as_current_user(self, enabled=True):
         self.json_put_data['enable_login_as_current_user'] = enabled
-        response = self.horizon_server.http_put(self.url, json=self.json_put_data)
+        response = self.horizon_server.http_put(
+            self.url, json=self.json_put_data)
         if response.status_code == 204:
             # sync data change
             self.json_get_data['enable_login_as_current_user'] = enabled
@@ -186,7 +196,8 @@ class GSSAPIAuthenticator:
     @allow_legacy_clients.setter
     def allow_legacy_clients(self, enabled=True):
         self.json_put_data['allow_legacy_clients'] = enabled
-        response = self.horizon_server.http_put(self.url, json=self.json_put_data)
+        response = self.horizon_server.http_put(
+            self.url, json=self.json_put_data)
         if response.status_code == 204:
             # sync data change
             self.json_get_data['allow_legacy_clients'] = enabled
@@ -214,7 +225,8 @@ class ConnectionServer:
     def __init__(self, connection_server_id, horizon_server: HorizonServer):
         self.id = connection_server_id
         self.horizon_server = horizon_server
-        self.url = 'https://{}/rest/config/v1/connection-servers/{}'.format(horizon_server.address, self.id)
+        self.url = 'https://{}/rest/config/v1/connection-servers/{}'.format(
+            horizon_server.address, self.id)
         self._generate_get_response_data()
 
     def __str__(self):
